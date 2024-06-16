7f2f2ff8629bdec95bfa632f6fa260b9d3dde602
Fabian <fabian@fmbb.se>
2024-06-16 14:35:27 +0200

    better test script

--- test.sh
+++ test.sh
@@ -17,43 +17,53 @@ then
     exit 1
 fi
 
-# Start the test server
 ($testbin > $testout 2>&1) &
 pid=$!
 
-# Wait for it to start
+printf "Waiting for server to start "
 while ! grep -q "Listening on" $testout
 do
     sleep 1
+    printf "."
 done
+printf " ready\n"
 
 test_failed=no
-
-# Run the test
-wrk -d2 'http://localhost:3000' > wrk.output
 
 curlcheck()
 {
     url=$1
     expected=$2
+    echo "Checking $url"
     actual=$(curl -s "$url")
     if ! echo "$actual" | grep -q "$expected"
     then
-        echo "Expected output not found in server response: $url"
+        echo "Expected output not found in server response"
         echo "Expected: $expected"
         echo "Actual: $actual"
         test_failed=yes
     fi
 }
 
+# Run the tests
+
+case "$@" in
+    --fast)
+        echo "Skipping wrk test"
+        ;;
+    *)
+        duration=2
+        printf "Running wrk test for %s seconds ..." $duration
+        wrk -d$duration 'http://localhost:3000' > wrk.output
+        printf " done\n"
+        ;;
+esac
+
 curlcheck 'http://localhost:3000' 'Hello, world!'
 curlcheck 'http://localhost:3000/goober' 'Hello, goober!'
 curlcheck 'http://localhost:3000/foo?style=quiet' 'hi foo'
 curlcheck 'http://localhost:3000/bar?style=loud' 'HELLO, BAR!!!'
 curlcheck 'http://localhost:3000/baz?style=nope' 'Bad style'
-
-# Clean up
-kill $pid
 
 # Check the output
 if grep -q "ERROR" $testout
@@ -62,11 +72,13 @@ then
     test_failed=yes
 fi
 
+kill $pid
+
 if [ $test_failed = yes ]
 then
     echo "Test failed"
-    #echo "Server output:"
-    #cat $testout
+    echo "Server output:"
+    cat $testout
     exit 1
 fi
 
